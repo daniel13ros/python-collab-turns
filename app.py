2429103e2f974aa655081f7f69509f65a768db56
Amir Shacham <shacham.amir@gmail.com>
2026-03-28 11:59:35 +0300

    B: replaced deleted entry

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # app.py
-from utils import add_entry, read_entries
+from utils import add_entry, delete_last_entry, read_entries
 
 add_entry("A: refining update")
 add_entry("Project initialized by B") 
+
+delete_last_entry()
+add_entry("B: replaced deleted entry")
+
 for line in read_entries():
     print(line)
